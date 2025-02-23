f1255f2cfb44fd307e09d322589cfa697f3925ff
dss16694 <dss16694@163.com>
2025-02-23 11:20:54 +0800

    Update MainActivity.java
    尝试解决桌面不显示状态栏时打开音乐时卡死的问题

--- android/app/src/main/java/cn/toside/music/mobile/MainActivity.java
+++ android/app/src/main/java/cn/toside/music/mobile/MainActivity.java
@@ -6,8 +6,20 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends NavigationActivity {
+    @Override
+    protected void onresume() {
+        ensureStatusBarVisible();
+        super.onresume();
+    }
 
-
-
-
+    private void ensureStatusBarVisible() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.show(WindowInsets.Type.statusBars());
+            }
+        } else {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+    }
 }
